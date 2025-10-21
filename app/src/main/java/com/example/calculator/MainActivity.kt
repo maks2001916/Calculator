2d69e7e1eac2f4fe28d83cf50e943002d4d9a71e
max <mamonov.maksim.20003@gmail.com>
2025-10-21 12:21:38 +0500

    Добавлена привязка новых элементов UI в коде. Добавлены функции конвертации систем счисления и обработки базовыми операциями чисел в разных системах счисления, в том числе и дробных.

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,10 +7,12 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.GridLayout
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
@@ -18,9 +20,13 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private var currentBase = 10 // Текущая система счисления (по умолчанию десятичная)
+    private var convertedValue = "" // Последнее сконвертированное значение
+
     private lateinit var toolbarTB: Toolbar
     private lateinit var inputET: EditText
     private lateinit var resultTV: TextView
+    //
     private lateinit var oneBTN: Button
     private lateinit var twoBTN: Button
     private lateinit var threeBTN: Button
@@ -31,12 +37,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var eightBTN: Button
     private lateinit var nineBTN: Button
     private lateinit var zeroBTN: Button
+    private lateinit var aBTN: Button
+    private lateinit var bBTN: Button
+    private lateinit var cBTN: Button
+    private lateinit var dBTN: Button
+    private lateinit var eBTN: Button
+    private lateinit var fBTN: Button
+
     private lateinit var divBTN: Button
     private lateinit var mulBTN: Button
     private lateinit var subBTN: Button
     private lateinit var sumBTN: Button
+    private lateinit var dotBTN: Button
+
     private lateinit var equBTN: Button
     private lateinit var resBTN: Button
+    private lateinit var binTV: TextView
+    private lateinit var decTV: TextView
+    private lateinit var octTV: TextView
+    private lateinit var hexTV: TextView
+    private lateinit var binSC: SwitchCompat
+    private lateinit var decSC: SwitchCompat
+    private lateinit var octSC: SwitchCompat
+    private lateinit var hexSC: SwitchCompat
+
+
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,12 +87,26 @@ class MainActivity : AppCompatActivity() {
         sumBTN = findViewById(R.id.sumBTN)
         equBTN = findViewById(R.id.equBTN)
         resBTN = findViewById(R.id.resBTN)
+        binTV = findViewById(R.id.binTV)
+        decTV = findViewById(R.id.decTV)
+        octTV = findViewById(R.id.octTV)
+        hexTV = findViewById(R.id.hexTV)
+        binSC = findViewById(R.id.binSC)
+        decSC = findViewById(R.id.decSC)
+        octSC = findViewById(R.id.octSC)
+        hexSC = findViewById(R.id.hexSC)
+
 
         setSupportActionBar(toolbarTB)
 
         //toolbarTB.setNavigationIcon(R.drawable.exit_fill)
 
-
+        fun setupNumberSystemButtons() {
+            binSC.setOnClickListener { convertNumber(2) }
+            octSC.setOnClickListener { convertNumber(8) }
+            decSC.setOnClickListener { convertNumber(10) }
+            hexSC.setOnClickListener { convertNumber(16) }
+        }
 
         oneBTN.setOnClickListener { setFigure("1") }
         twoBTN.setOnClickListener { setFigure("2") }
@@ -80,10 +119,19 @@ class MainActivity : AppCompatActivity() {
         nineBTN.setOnClickListener { setFigure("9") }
         zeroBTN.setOnClickListener { setFigure("0") }
 
+        aBTN.setOnClickListener { setFigure("A") }
+        bBTN.setOnClickListener { setFigure("B") }
+        cBTN.setOnClickListener { setFigure("C") }
+        dBTN.setOnClickListener { setFigure("D") }
+        eBTN.setOnClickListener { setFigure("E") }
+        fBTN.setOnClickListener { setFigure("F") }
+        dotBTN.setOnClickListener { setFigure(".") }
+
         divBTN.setOnClickListener { setFigure("/") }
         mulBTN.setOnClickListener { setFigure("*") }
         subBTN.setOnClickListener { setFigure("-") }
         sumBTN.setOnClickListener { setFigure("+") }
+
         equBTN.setOnClickListener {
             setFigure("=")
             if (checkingForAnExample(inputET.text.toString())) {
@@ -142,10 +190,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkingForAnExample(string: String) : Boolean {
-        val regex = Regex("^[0-9]+[*/\\+\\-][0-9]+[ =]$")
-
-        if (string.matches(regex)) return true
-        return false
+        val regex = Regex("^[0-9A-Fa-f.]+[*/\\+\\-][0-9A-Fa-f.]+[ =]$")
+        return string.matches(regex)
     }
 
     fun getFirstPart(input: String): String {
@@ -153,9 +199,7 @@ class MainActivity : AppCompatActivity() {
         val operatorIndex = input.indexOf(operator)
         return if (operatorIndex != -1) {
             input.substring(0, operatorIndex).trim()
-        } else {
-            ""
-        }
+        } else { "" }
     }
 
     fun getSecondPart(input: String): String {
@@ -167,6 +211,160 @@ class MainActivity : AppCompatActivity() {
             return input.substring(operatorIndex + 1, endIndex).trim()
         }
         return ""
+    }
+
+    fun convertNumber(newBase: Int) {
+        val inputText = inputET.text.toString()
+
+        if (inputText.isNotEmpty() && !inputText.contains("[+\\-*/]".toRegex())) {
+            try {
+                val valueToConvert = if
+                        (resultTV.text.toString() != getString(R.string.result) &&
+                        !resultTV.text.toString().startsWith("Ошибка")) {
+                    resultTV.text.toString()
+                } else {
+                    inputText
+                }
+                val decimalValue = convertToDecimal(valueToConvert, currentBase)
+                val converted = convertFromDecimal(decimalValue, currentBase)
+
+                inputET.setText(converted)
+                convertedValue = converted
+                currentBase = newBase
+            } catch (e: Exception) {
+                Toast.makeText(
+                    applicationContext,
+                    "Ошибка конвертации: ${e.message}",
+                    Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else if (convertedValue.isNotEmpty()) {
+            inputET.setText(convertedValue)
+        }
+
+        currentBase = newBase
+    }
+
+    fun convertToDecimal(number: String, fromBase: Int): Double {
+        if (fromBase == 10) {
+            return number.toDoubleOrNull() ?: throw IllegalArgumentException("Некорректное десятичное число")
+        }
+
+        var cleanNumber = number.uppercase()
+        if (cleanNumber.startsWith("0X") ||
+            cleanNumber.startsWith("0B") ||
+            cleanNumber.startsWith("0O"))
+            cleanNumber = cleanNumber.substring(2)
+
+        return  if (cleanNumber.contains('.')) {
+            val parts = cleanNumber.split('.')
+            val integerPart = convertIntegerPart(parts[0], fromBase)
+            val fractionalPart = convertFractionalPart(parts[1], fromBase)
+            integerPart + fractionalPart
+        } else {
+            convertIntegerPart(cleanNumber, fromBase).toDouble()
+        }
+    }
+
+    private fun convertIntegerPart(integerPart: String, fromBase: Int): Int {
+        if (integerPart.isEmpty()) return 0
+
+        var result = 0
+        val power = 1
+        for (i in integerPart.length - 1 downTo 0) {
+            val digit = charToDigit(integerPart[i])
+            if (digit >=fromBase) {
+                throw IllegalArgumentException("Цифра $digit недопустима в системе счисления $fromBase")
+            }
+            result += digit * power
+            result *= fromBase
+        }
+        return result
+    }
+
+    private fun convertFractionalPart(fractionalPart: String, fromBase: Int): Double {
+        if (fractionalPart.isEmpty()) return  0.0
+
+        var result = 0.0
+        var division = fromBase.toDouble()
+        for (i in fractionalPart.indices) {
+            val digit = charToDigit(fractionalPart[i])
+            if (digit >= fromBase) {
+                throw IllegalArgumentException("Цифра $digit не доступна в системе счисления $fromBase")
+            }
+            result += digit / division
+            division *= fromBase
+        }
+        return result
+    }
+
+    fun convertFromDecimal(decimalNumber: Double, toBase: Int): String {
+        if (toBase == 10) {
+            return if (decimalNumber % 1 == 0.0) {
+                decimalNumber.toInt().toString()
+            } else {
+                String.format("%.6f", decimalNumber).trimEnd('0').trimEnd('.')
+            }
+        }
+        val integerPart = decimalNumber.toInt()
+        val fractionalPart = decimalNumber - integerPart
+
+        val integerResult = convertIntegerPartFromDecimal(integerPart, toBase)
+
+        return if (fractionalPart > 0) {
+            val fractionalResult = convertFractionalPartFromDecimal(fractionalPart, toBase)
+            "$integerResult.$fractionalResult"
+        } else {
+            integerResult
+        }
+    }
+
+    private fun convertIntegerPartFromDecimal(number: Int, toBase: Int): String {
+        if (number == 0) return "0"
+
+        var n = number
+        val result = StringBuilder()
+
+        while (n > 0) {
+            val reminder = n % toBase
+            result.insert(0, digitToChar(reminder))
+            n /= toBase
+        }
+        return result.toString()
+    }
+
+    private fun convertFractionalPartFromDecimal(fraction: Double, toBase: Int, precision: Int = 6): String {
+        if (fraction == 0.0) return ""
+
+        var fractional = fraction
+        val result = StringBuilder()
+        var count = 0
+
+        while (fractional > 0 && count < precision) {
+            fractional *= toBase
+            val digit = fractional.toInt()
+            result.append(digitToChar(digit))
+            fractional -= digit
+            count++
+        }
+        return result.toString()
+    }
+
+    private fun charToDigit(char: Char): Int {
+        return when (char) {
+            in '0'..'9' -> char - '0'
+            in 'A'..'F' -> char - 'A' + 10
+            in 'a'..'f' -> char - 'a' + 10
+            else -> throw IllegalArgumentException("Недоаустимый символ: $char")
+        }
+    }
+
+    private fun digitToChar(digit: Int): Char {
+        return when (digit) {
+            in 0..9 -> '0' + digit
+            in 10..15 -> 'A' + (digit - 10)
+            else -> throw IllegalArgumentException("Недопустимая цифра: $digit")
+        }
     }
 
     fun getOperator(input: String): String {
